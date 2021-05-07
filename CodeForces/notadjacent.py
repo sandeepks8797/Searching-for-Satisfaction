2d70bfbbad2614860fbe3c9dad8a97a3673bb4d9
Sandeep <onlinework8797@gmail.com>
2021-05-07 14:46:27 +0530

    Not adjacent

--- CodeForces/notadjacent.py
+++ CodeForces/notadjacent.py
@@ -4,19 +4,17 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    mat = [[0]*n]*n
+    mat = [[0 for i in range(n)] for j in range(n)]
     temp = 1
     
     for i in range(n):
-        for j in range((i%2), n):
+        for j in range((i%2), n, 2):
             mat[i][j] = temp
-            j += 1
             temp += 1
     
     for x in range(n):
-        for y in range((x+1)%2 ,n):
+        for y in range((x+1)%2 ,n, 2):
             mat[x][y] = temp
-            y += 1
             temp += 1
     print(mat)
 
